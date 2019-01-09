daa915786af9f2276c78e5ff195ed655c5cfb949
Arm <chayanin.suatap@gmail.com>
2019-01-09 10:27:46 +0700

    *** fix remove input, download only undownloaded

--- download_icon.py
+++ download_icon.py
@@ -4,6 +4,8 @@ import db_util
 import scrapy
 from scrapy_util import get_app_id
 from scrapy.crawler import CrawlerProcess
+import os.path
+from time import sleep
 
 def download_icon(app_id, resp):
     try :
@@ -18,19 +20,35 @@ def download_icon(app_id, resp):
     except :
         print('error :', app_id)
 
+def _get_downloaded_icon_app_ids():
+    output = []
+    for fn in os.listdir( mypath.icon_folder) :
+        output.append( fn[:-4])
+    return output
+
+def _get_undownloaded_icon_app_ids():
+    conn = db_util.connect_db()
+    app_ids = db_util.get_all_app_id(conn)
+    undownloaded = _get_downloaded_icon_app_ids()
+    for x in undownloaded :
+        app_ids.remove(x)
+    return app_ids
+
 class IconDownloader( scrapy.Spider):
     name = 'Joe'
     def start_requests(self) :
         main_link = 'https://play.google.com/store/apps/details?id='
-        conn = db_util.connect_db()
-        app_ids = db_util.get_all_app_id(conn)
+        app_ids = _get_undownloaded_icon_app_ids()
+
+        print('undownloaded amount:', len(app_ids))
+        sleep(2)
+
         for app_id in app_ids :
              yield scrapy.Request( main_link + app_id, self.parse)
     
     def parse(self, resp) :
         app_id = get_app_id(resp.url)
         download_icon(app_id, resp)
-        input()
 
 if __name__ == '__main__' :
     proc = CrawlerProcess({
@@ -39,3 +57,4 @@ if __name__ == '__main__' :
 
     proc.crawl(IconDownloader)
     proc.start()
+
