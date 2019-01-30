3a76160abd901cd37697aea311041aa5c76d3136
Arm <chayanin.suatap@gmail.com>
2019-01-30 23:43:34 +0700

    add train regression

--- overall_feature_util.py
+++ overall_feature_util.py
@@ -14,7 +14,7 @@ def extract_feature_vec(rec , use_download_amount = True, use_rating_amount = Tr
     screenshots_amount = rec[9]
     content_rating = rec[10]
 
-    rating = 0 if float(rating) < 4.5 else 1
+    rating = float(rating)
     download_amount = _extract_download_amount(download_amount)
     category = _extract_category(category)
     price = 0 if price == 'free' else 1
--- overall_train.py
+++ overall_train.py
@@ -4,12 +4,14 @@ import random
 from keras.layers import Input, Dense
 from keras.models import Model
 import numpy as np
-from keras.utils.np_utils import to_categorical
+from keras.layers import Activation
+from keras import backend as K
+from keras.utils.generic_utils import get_custom_objects
 
 #extract feature vector
 vec_and_labels = []
 for record in overall_db_util.query() :
-    t = extract_feature_vec(record, use_download_amount=False, use_rating_amount=False, use_in_app_products=False)
+    t = extract_feature_vec(record, use_content_rating=False, use_category=False, use_app_version=False, use_in_app_products=False, use_sdk_version=True, use_screenshots_amount=False, use_rating_amount=True, use_last_update_date=False, use_price=False, use_download_amount=True)
     vec_and_labels.append(t)
 
 #shuffle
@@ -30,8 +32,8 @@ ninety = int(len(vec_and_labels) * 90 / 100)
 x90 = np.asarray(featvec[:ninety])
 x10 = np.asarray(featvec[ninety:])
 print('test y ratio :', label[ninety:].count(0), label[ninety:].count(1))
-y90 = to_categorical(np.asarray(label[:ninety]), output_shape)
-y10 = to_categorical(np.asarray(label[ninety:]), output_shape)
+y90 = np.asarray([[x] for x in label[:ninety]])
+y10 = np.asarray([[x] for x in label[ninety:]])
 
 #make model
 input_shape = len(x90[0])
@@ -42,9 +44,15 @@ layer_input = Input(shape=(input_shape,), name='overall_input')
 t = Dense( dense_size, activation='relu' )(layer_input)
 for i in range(10) :
     t = Dense(dense_size, activation='relu')(t)
-layer_output = Dense(2, activation='softmax', name='overall_output')(t)
+
+#custom activation
+def my_sigmoid(x):
+    return (K.sigmoid(x) * 5)
+get_custom_objects().update({'my_sigmoid': Activation(my_sigmoid)})
+
+layer_output = Dense(1, activation='my_sigmoid', name='overall_output')(t)
 
 model = Model(inputs = layer_input, outputs = layer_output)
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='mse', metrics=['MAE','MSE'])
 
-model.fit(x90, y90, validation_data=(x10, y10), epochs=10, batch_size=32)    
+model.fit(x90, y90, validation_data=(x10, y10), epochs=20, batch_size=32)    
