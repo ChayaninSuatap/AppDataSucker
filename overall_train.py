96e853dfc88db3113b9cf4b33cd8793472c2970c
Arm <chayanin.suatap@gmail.com>
2019-02-13 10:03:39 +0700

    refac plotting

--- overall_train.py
+++ overall_train.py
@@ -3,8 +3,9 @@ from overall_feature_util import extract_feature_vec
 import random
 import numpy as np
 from keras.utils.np_utils import to_categorical
-from overall_util import save_prediction_to_file, save_testset_labels_to_file, prepare_dataset, create_model, plot_loss
+from overall_util import save_prediction_to_file, save_testset_labels_to_file, prepare_dataset, create_model
 from keras.callbacks import ModelCheckpoint
+from plt_util import plot_loss
 
 x_category_90, x_sdk_version_90, x_content_rating_90, x_other_90, y_90, \
     x_category_10, x_sdk_version_10, x_content_rating_10, x_other_10, y_10 = prepare_dataset()
@@ -21,7 +22,7 @@ model = create_model(input_other_shape=other_shape, input_category_shape=categor
     input_content_rating_shape=content_rating_shape, \
     category_densed_shape=10, sdk_version_densed_shape=10, content_rating_densed_shape=10, dense_level=5, num_class=4)
 checkpoint_path = 'overall_ep_{epoch:03d}_loss_{loss:.2f}_val_loss_{val_loss:.2f}_val_acc_{val_acc:.2f}.hdf5'
-checkpoint = ModelCheckpoint(checkpoint_path, monitor='val_acc', save_best_only=True)
+checkpoint = ModelCheckpoint(checkpoint_path, monitor='val_acc', save_best_only=False)
 history = model.fit([x_category_90, x_sdk_version_90, x_content_rating_90, x_other_90], y_90, \
-    validation_data=([x_category_10, x_sdk_version_10, x_content_rating_10, x_other_10], y_10), epochs=10, batch_size=32, callbacks=[checkpoint])
+    validation_data=([x_category_10, x_sdk_version_10, x_content_rating_10, x_other_10], y_10), epochs=100, batch_size=32, callbacks=[checkpoint])
 plot_loss(history)
--- overall_util.py
+++ overall_util.py
@@ -4,9 +4,6 @@ from overall_feature_util import extract_feature_vec, normalize_number
 import random
 from keras.layers import Input, Dense, concatenate
 from keras.models import Model
-import matplotlib
-matplotlib.use('agg')
-import matplotlib.pyplot as plt
 
 def save_prediction_to_file(model, dataset, batch_size):
     answers = [x[0] for x in model.predict(dataset, batch_size=batch_size)]
@@ -22,24 +19,6 @@ def save_testset_labels_to_file(testset):
     f = open('testset_labels.txt','w')
     for x in testset:
         f.write(str(x)+'\n')
-
-def plot_loss(history):
-    plt.plot(history.history['acc'])
-    plt.plot(history.history['val_acc'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.savefig('graph_acc.png')
-    plt.clf()
-    # summarize history for loss
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.savefig('graph_loss.png')
 
 def prepare_dataset(use_download_amount=True, use_rating_amount=True, testset_percent=90):
     #query from db
