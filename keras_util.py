c2a139369694832fdd4d3236106127e8717b594a
Arm <chayanin.suatap@gmail.com>
2019-04-08 20:37:07 +0700

    add plot weights callback

--- keras_util.py
+++ keras_util.py
@@ -29,6 +29,8 @@ def group_for_fit_generator(xs, n):
 
 from plt_util import plot_confusion_matrix
 from keras.callbacks import Callback
+import numpy as np
+import math
 class PlotConfusionMatrixCallback(Callback):
     def set_postfix_name(self, name):
         self.postfix_name = name
@@ -38,3 +40,28 @@ class PlotConfusionMatrixCallback(Callback):
         y_test = self.validation_data[1]
         plot_confusion_matrix(self.model, (x_test,y_test), 32,
             fn_postfix=self.postfix_name + '_ep_' + str(epoch+1), shut_up=True)
+
+def _get_weights_of_layers(model):
+    output = []
+    for layer in model.layers:
+        w_and_b = layer.get_weights()
+        if w_and_b != []:
+            weights = w_and_b[0].flatten()
+            biases = w_and_b[1].flatten()
+            output.append( np.append(weights,biases))
+    return output
+
+class PlotWeightsCallback(Callback):
+    def on_train_begin(self, logs={}):
+        self.weights_last_epoch = _get_weights_of_layers(self.model)
+    
+    def on_epoch_end(self, epoch, logs={}):
+        weights_this_epoch = _get_weights_of_layers(self.model)
+        diff_weights = []
+        for old, new in zip(self.weights_last_epoch, weights_this_epoch):
+            diff_weights.append(np.absolute(old-new).sum())
+        print('weight diff',diff_weights)
+        self.weights_last_epoch = weights_this_epoch
+
+
+
