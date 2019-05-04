d3b9f5ff09173d9c69b35f9509634590befd3547
Arm <chayanin.suatap@gmail.com>
2019-05-04 22:05:20 +0700

    fix conv1x1 has no relu

--- icon_my_model.py
+++ icon_my_model.py
@@ -2,7 +2,7 @@ import numpy as np
 import db_util
 import random
 from keras_util import compute_class_weight, group_for_fit_generator, PlotAccLossCallback, create_image_data_gen
-from keras.layers import Dense, Conv2D, Input, MaxPooling2D, Flatten, Dropout, BatchNormalization, ReLU
+from keras.layers import Dense, Conv2D, Input, MaxPooling2D, Flatten, Dropout, BatchNormalization, ReLU, LeakyReLU
 from keras.models import Model
 from datetime import datetime
 import icon_util
@@ -24,8 +24,8 @@ for x in dat:
 #random shuffle 
 time_seed = int(datetime.now().microsecond)
 random.seed(time_seed)
-random.seed(21)
-np.random.seed(21)
+random.seed(7)
+np.random.seed(7)
 
 print('time_seed',time_seed)
 random.shuffle(app_ids_and_labels)
@@ -64,21 +64,23 @@ else:
     # input()
 #make model
 input_layer = Input(shape=(128, 128, 3))
-# x = Conv2D(8,(3,3), activation='relu', name='my_model_conv_0', kernel_initializer='glorot_uniform')(input_layer)
-# x = MaxPooling2D((2,2), name='my_model_max_pooling_0')(x)
-# x = Conv2D(16,(3,3), activation='relu', name='my_model_conv_1', kernel_initializer='glorot_uniform')(input_layer)
-# x = MaxPooling2D((2,2), name='my_model_max_pooling_1')(x)
 x = Conv2D(32,(3,3), name='my_model_conv_2')(input_layer)
 x = ReLU()(x)
-# x = MaxPooling2D((2,2), name='my_model_max_pooling_2')(x)
 x = Dropout(0.1)(x)
+# x = MaxPooling2D((2,2), name='my_model_max_pooling_2')(x)
 x = Conv2D(64,(3,3), name='my_model_conv_3')(x)
 x = ReLU()(x)
 x = Dropout(0.1)(x)
+
+x = Conv2D(32,(1,1), name='my_model_conv_11')(x)
+x = ReLU()(x)
+
 x = MaxPooling2D((2,2), name='my_model_max_pooling_3')(x)
-# x = Conv2D(128,(3,3), activation='relu', name='my_model_conv_4', kernel_initializer='glorot_uniform')(x)
+# x = Conv2D(128,(3,3), name='my_model_conv_4')(x)
+# x = Dropout(0.1)(x)
+# x = Conv2D(256,(3,3), name='my_model_conv_5')(x)
+# x = Dropout(0.1)(x)
 # x = MaxPooling2D((2,2), name='my_model_max_pooling_4')(x)
-# x = Conv2D(256,(3,3), activation='relu', name='my_model_conv_5', kernel_initializer='glorot_uniform')(x)
 x = Flatten(name='my_model_flatten')(x)
 # custom activation fn
 def my_sigmoid(x): return (K.sigmoid(x)*5)
@@ -89,14 +91,16 @@ get_custom_objects().update({'my_sigmoid':act})
 if IS_REGRESSION:
     x = Dense(1, activation='my_sigmoid', name='my_model_regress_1')(x)
 else:
-    x = Dense(4, activation='softmax', name='my_model_dense_2', kernel_initializer='glorot_uniform')(x)
+    # x = Dense(16, activation='softmax', name='my_model_dense_2', kernel_initializer='glorot_uniform')(x)
+    x = Dense(4, activation='softmax', name='my_model_dense_3', kernel_initializer='glorot_uniform')(x)
 model = Model(input=input_layer, output=x)
 #compile
 if IS_REGRESSION:
     model.compile(loss='mse', optimizer='adam', metrics=['mae'])
 else:
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
-
+model.summary()
+input()
 #generator
 epochs = 999
 batch_size = 32
@@ -157,7 +161,7 @@ def test_generator():
             yield icons, labels
 
 # write save each epoch
-filepath='armnet_oversample_augment-ep-{epoch:03d}-loss-{loss:.3f}-acc-{acc:.3f}-vloss-{val_loss:.3f}-vacc-{val_acc:.3f}.hdf5'
+filepath='armnet_augment_dropout_conv11-ep-{epoch:03d}-loss-{loss:.3f}-acc-{acc:.3f}-vloss-{val_loss:.3f}-vacc-{val_acc:.3f}.hdf5'
 if IS_REGRESSION:
     filepath='armnet_regression-ep-{epoch:03d}-loss-{loss:.3f}-vloss-{val_loss:.3f}-vmas-{val_mean_absolute_error:.3f}.hdf5'
 checkpoint = ModelCheckpoint(filepath, monitor='val_acc', save_best_only=False, verbose=0, period=1)
