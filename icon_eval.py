5e81e3a379d632ffa49209a29c109f5c54769cf7
Arm <chayanin.suatap@gmail.com>
2019-05-14 19:44:57 +0700

    icon eval 3 class

--- icon_eval.py
+++ icon_eval.py
@@ -9,8 +9,7 @@ from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 IS_REGRESSION = False
 model_path = \
-'armnet_3_class-ep-136-loss-0.435-acc-0.821-vloss-2.193-vacc-0.418.hdf5'
-# 'armnet_3_class-ep-024-loss-0.838-acc-0.610-vloss-1.224-vacc-0.425.hdf5'
+'armnet_oversample_augment-ep-070-loss-0.637-acc-0.747-vloss-2.207-vacc-0.358.hdf5'
 
 # prepare x y
 conn = db_util.connect_db()
@@ -19,33 +18,35 @@ dat=conn.execute('select app_id, rating from app_data')
 for x in dat:
     if x[1] != None:
       app_ids_and_labels.append( (x[0], x[1]))  
-random.seed(7)
-np.random.seed(7)
+random.seed(21)
+np.random.seed(21)
 random.shuffle(app_ids_and_labels)
 ninety = int(len(app_ids_and_labels)*80/100)
 
 #split class
 for i in range(len(app_ids_and_labels)):
     app_id , rating = app_ids_and_labels[i]
-    # if float(rating) <= 3.5: rating = 0
-    # elif float(rating) > 3.5 and float(rating) <= 4.0: rating = 1
-    # elif float(rating) > 4.0 and float(rating) <= 4.5: rating = 2
-    # else: rating = 3
-    if float(rating) <= 3.9: rating = 0
-    elif float(rating) > 3.9 and float(rating) <= 4.4: rating = 1
-    # elif float(rating) > 4.0 and float(rating) <= 4.5: rating = 2
-    else: rating = 2
+    if float(rating) <= 3.5: rating = 0
+    elif float(rating) > 3.5 and float(rating) <= 4.0: rating = 1
+    elif float(rating) > 4.0 and float(rating) <= 4.5: rating = 2
+    else: rating = 3
+
+    #3 class
+    # if float(rating) <= 3.9: rating = 0
+    # elif float(rating) > 3.9 and float(rating) <= 4.4: rating = 1
+    # else: rating = 2
+
     if IS_REGRESSION: rating = float(app_ids_and_labels[i][1])
     app_ids_and_labels[i] = app_id, rating
 xs = []
 ys = []
 
 #show dist
-dist = {0:0,1:0,2:0}
+dist = {0:0,1:0,2:0,3:0}
 for _,x in app_ids_and_labels[:ninety]:
     dist[x]+=1
 print('train dist', dist)
-dist = {0:0,1:0,2:0}
+dist = {0:0,1:0,2:0,3:0}
 for _,x in app_ids_and_labels[ninety:]:
     dist[x]+=1
 print('test dist', dist)
@@ -64,7 +65,7 @@ xs /= 255
 
 #evaluate
 if not IS_REGRESSION:
-    ys = to_categorical(ys, 3)
+    ys = to_categorical(ys, 4)
     plot_confusion_matrix(model_path, (xs,ys), 
         batch_size=64, fn_postfix='')
 else:
