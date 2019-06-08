26bfcc3068693dc8116e0ee7094b7b4a62f326d5
Arm <chayanin.suatap@gmail.com>
2019-06-08 10:09:19 +0700

    add predict comment

--- icon_cate.py
+++ icon_cate.py
@@ -2,9 +2,11 @@ import preprocess_util
 import icon_cate_util
 import numpy as np
 import random
+import icon_util
 import math
 from keras.callbacks import ModelCheckpoint
 from keras_util import PlotAccLossCallback, gen_k_fold_pass
+from keras.models import load_model
 
 random.seed(281)
 np.random.seed(281)
@@ -26,6 +28,25 @@ batch_size = 24
 epochs = 999
 gen_train = icon_cate_util.datagenerator(aial_train, batch_size, epochs, cate_only=True)
 gen_test = icon_cate_util.datagenerator(aial_test, batch_size, epochs, cate_only=True)
+
+#predict
+# model = load_model('reg_cate_only_k0-ep-006-loss-0.173-acc0.946-vloss-0.186-vacc-0.945.hdf5')
+# f = open('pred.txt', 'w')
+# f2 = open('real.txt', 'w')
+# for x in aial_test:
+#     app_id = x[0]
+#     answer = x[2]
+#     try:
+#         icon = icon_util.load_icon_by_app_id(x[0], 128, 128)
+#     except Exception as e:
+#         pass
+#     finally:
+#         icons = np.array([icon])
+#         icons = icons.astype(np.float)
+#         icons /= 255  
+#         pred = model.predict(icons)
+#         f.writelines(str(pred[0].tolist()) + '\n')
+#         f2.writelines(str(answer) + '\n')
 
 filepath='reg_cate_only_k0-ep-{epoch:03d}-loss-{loss:.3f}-acc{acc:.3f}-vloss-{val_loss:.3f}-vacc-{val_acc:.3f}.hdf5'
 ### save for predict rating + cate
