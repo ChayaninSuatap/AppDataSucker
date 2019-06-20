744abf20a24c9cd642272490dce113e5f82a5e24
Arm <chayanin.suatap@gmail.com>
2019-06-20 10:48:51 +0700

    gabage : sc_cate gen result

--- sc_cate.py
+++ sc_cate.py
@@ -23,7 +23,17 @@ icon_cate_util.check_aial_error(aial)
 #filter only rating cate
 aial = preprocess_util.get_app_id_rating_cate_from_aial(aial)
 
-aial_train, aial_test = gen_k_fold_pass(aial, kf_pass=0, n_splits=4)
+for i in range(4):
+    sum_train = [0] * 17
+    sum_test = [0] * 17
+    aial_train, aial_test = gen_k_fold_pass(aial, kf_pass=i, n_splits=4)
+    for x in aial_train:
+        sum_train[x[2].index(1)] += 1
+    for x in aial_test:
+        sum_test[x[2].index(1)] += 1
+    print(sum_train)
+    print(sum_test)
+    input()
 print(icon_cate_util.compute_baseline(aial_train, aial_test))
 
 #make aial_train_sc, aial_test_sc
@@ -36,7 +46,44 @@ gen_train=icon_cate_util.datagenerator(aial_train_sc,
 gen_test=icon_cate_util.datagenerator(aial_test_sc,
         batch_size, epochs, cate_only=True, train_sc=True)
 
-model=icon_cate_util.create_icon_cate_model(cate_only=True, is_softmax=True, train_sc=True)
+elem = max( [(k,v) for k,v in sc_dict.items()], key=lambda x: len(x[1]))
+print(elem)
+input()
+from keras.models import load_model
+import icon_util
+model = load_model('sc_cate_only_softmax_k0-ep-061-loss-0.249-acc-0.916-vloss-3.464-vacc-0.359.hdf5')
+for app_id, _ , label in aial_test:
+    if (app_id in sc_dict) == False: continue
+    truth = np.array(label).argmax()
+    #output
+    print(app_id, truth, end=' ')
+    file_out = app_id + ' ' + str(truth) + ' '
+
+    for ss_fn in sc_dict[app_id]:
+        try:
+            icon = icon_util.load_icon_by_fn(mypath.screenshot_folder+ss_fn, 256, 160, rotate_for_sc=True)
+        except:
+            continue
+        icon = icon.astype('float32')
+        icon /= 255
+        pred = model.predict(np.array([icon]))
+        t = pred[0].argmax()
+        #output
+        print(t, end=' ')
+        file_out += str(t) + ' '
+    #output
+    print('')
+    f = open('sc_fold1_testset.txt', 'a')
+    f.writelines(file_out + '\n')
+    f.close()
+
+input('done')
+
+            
+
+
+
+# model=icon_cate_util.create_icon_cate_model(cate_only=True, is_softmax=True, train_sc=True)
 filepath='sc_cate_only_softmax_k0-ep-{epoch:03d}-loss-{loss:.3f}-acc{acc:.3f}-vloss-{val_loss:.3f}-vacc-{val_acc:.3f}.hdf5'
 
 checkpoint = ModelCheckpoint(filepath, monitor='val_acc', save_best_only=False, verbose=0, period=1)
