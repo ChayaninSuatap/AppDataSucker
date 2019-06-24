22ee9a087a572eab67b2a40d3d8aa1cd31acd4f8
Arm <chayanin.suatap@gmail.com>
2019-06-24 16:50:46 +0700

    sc data export compute type 4

--- sc_data_export.py
+++ sc_data_export.py
@@ -56,47 +56,53 @@ def compute_mode_from_spreadsheet_txt(k_iter):
             else:
                 prob_dict[class_label] += pred_prob
             #prob dict got total not average
-
-        #counter
-        counter = collections.Counter(labels)
-        ls = []
-        for elem, freq in counter.items():
-            ls.append(( elem, freq))
-        #make prob_dict average
-        for k in prob_dict.keys():
-            prob_dict[k] = prob_dict[k] / counter[k]
-        #get max pred occurrence
-        ls_sorted = sorted(ls, key=lambda x: x[1], reverse=True)
-        #compute mode
-        occur_prob = [0.03965584164,0.01720791823,0.09011515073,0.05776943977,0.007892353474,0.03448052788,0.08008798033,0.1359813689,0.07161340406,0.04948893777,0.06035709665,0.03752102471,0.05647561133,0.0478069608,0.03525682495,0.1183853021,0.0599042567]
-        def compute_mode(ls_sorted):
-            if len(ls_sorted) == 0:
-                return -1
-            elif len(ls_sorted) == 1:
-                return ls_sorted[0][0]
-            #case top two freq is equal
-            elif ls_sorted[0][1] == ls_sorted[1][1]: 
-                elem0 = ls_sorted[0][0]
-                elem1 = ls_sorted[1][0]
-                #decide by occurrence in dataset
-                # if occur_prob[elem0] > occur_prob[elem1]:
-                #     return elem0
-                # else:
-                #     return elem1
-
-                #decide by pred prob
-                if prob_dict[elem0] * occur_prob[elem0] > prob_dict[elem1] * occur_prob[elem1]:
-                    return elem0
-                else:
-                    return elem1
-                
-            elif ls_sorted[0][1] == ls_sorted[1][1] and ls_sorted[0][1] == ls_sorted[2][1]:
-                raise ValueError('THREE OCCURRENCE EQUAL')
-                input()
-            else:
-                return ls_sorted[0][0]
         
-        mode = compute_mode(ls_sorted)
+        #compute mode for 4 type
+        try:
+            mode = max(prob_dict.items(), key = lambda x : x[1])[0]
+        except:
+            mode = -1
+        
+        # compute mode for first 3 type
+        # #counter
+        # counter = collections.Counter(labels)
+        # ls = []
+        # for elem, freq in counter.items():
+        #     ls.append(( elem, freq))
+        # #make prob_dict average
+        # for k in prob_dict.keys():
+        #     prob_dict[k] = prob_dict[k] / counter[k]
+        # #get max pred occurrence
+        # ls_sorted = sorted(ls, key=lambda x: x[1], reverse=True)
+        # #compute mode
+        # occur_prob = [0.03965584164,0.01720791823,0.09011515073,0.05776943977,0.007892353474,0.03448052788,0.08008798033,0.1359813689,0.07161340406,0.04948893777,0.06035709665,0.03752102471,0.05647561133,0.0478069608,0.03525682495,0.1183853021,0.0599042567]
+        # def compute_mode(ls_sorted):
+        #     if len(ls_sorted) == 0:
+        #         return -1
+        #     elif len(ls_sorted) == 1:
+        #         return ls_sorted[0][0]
+        #     #case top two freq is equal
+        #     elif ls_sorted[0][1] == ls_sorted[1][1]: 
+        #         elem0 = ls_sorted[0][0]
+        #         elem1 = ls_sorted[1][0]
+        #         #decide by occurrence in dataset
+        #         # if occur_prob[elem0] > occur_prob[elem1]:
+        #         #     return elem0
+        #         # else:
+        #         #     return elem1
+
+        #         #decide by pred prob
+        #         if prob_dict[elem0] * occur_prob[elem0] > prob_dict[elem1] * occur_prob[elem1]:
+        #             return elem0
+        #         else:
+        #             return elem1
+                
+        #     elif ls_sorted[0][1] == ls_sorted[1][1] and ls_sorted[0][1] == ls_sorted[2][1]:
+        #         raise ValueError('THREE OCCURRENCE EQUAL')
+        #         input()
+        #     else:
+        #         return ls_sorted[0][0]
+        # mode = compute_mode(ls_sorted)
         f_vote.writelines(str(mode)+'\n')
         print(mode)
     f_vote.close()
