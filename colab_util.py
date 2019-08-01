14475a69a40a092d65bbf4bcb048e5fd9862b0b8
Arm <chayanin.suatap@gmail.com>
2019-08-01 13:41:06 +0700

    fix : colab_util indent

--- colab_util.py
+++ colab_util.py
@@ -4,9 +4,9 @@ def get_training_latest_ep(proj, is_icon=True, is_sc=False):
     fns = os.listdir('/content/drive/My Drive/%s' % (proj,))
     ep_nums=[]
     for fn in fns:
-    if fn[-3:] == 'png':
-        ep_num = int(fn.split('.')[0])
-        ep_nums.append(ep_num)
+        if fn[-3:] == 'png':
+            ep_num = int(fn.split('.')[0])
+            ep_nums.append(ep_num)
     max_ep = max(ep_nums)
 
     if is_icon==True:
