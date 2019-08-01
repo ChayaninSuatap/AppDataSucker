25324082c91e08cf16492f76aa35cd4401e4b41f
Arm <chayanin.suatap@gmail.com>
2019-08-01 13:47:18 +0700

    fix : colab_util

--- colab_util.py
+++ colab_util.py
@@ -1,13 +1,17 @@
 import os
 
-def get_training_latest_ep(proj, is_icon=True, is_sc=False):
+def get_training_latest_ep(proj, is_icon=False, is_sc=False):
     fns = os.listdir('/content/drive/My Drive/%s' % (proj,))
     ep_nums=[]
     for fn in fns:
         if fn[-3:] == 'png':
             ep_num = int(fn.split('.')[0])
             ep_nums.append(ep_num)
-    max_ep = max(ep_nums)
+
+    if len(ep_nums) == 0:
+        return 0
+    else:
+        max_ep = max(ep_nums)
 
     if is_icon==True:
         return max_ep - (max_ep % 10)
