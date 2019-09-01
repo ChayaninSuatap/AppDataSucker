08208affb7fc3526490cacbaf46b473700a9b53a
Arm <chayanin.suatap@gmail.com>
2019-09-01 20:08:18 +0700

    add custom period option in colab

--- colab_util.py
+++ colab_util.py
@@ -1,6 +1,6 @@
 import os
 
-def get_training_latest_ep(proj, is_icon=False, is_sc=False):
+def get_training_latest_ep(proj, is_icon=False, is_sc=False, custom_period=None):
     fns = os.listdir('/content/drive/My Drive/%s' % (proj,))
     ep_nums=[]
     for fn in fns:
@@ -13,7 +13,9 @@ def get_training_latest_ep(proj, is_icon=False, is_sc=False):
     else:
         max_ep = max(ep_nums)
 
-    if is_icon==True:
+    if custom_period is not None:
+        return max_ep - (max_ep % custom_period)
+    elif is_icon==True:
         return max_ep - (max_ep % 10)
     elif is_sc==True:
         return max_ep - (max_ep % 4)
