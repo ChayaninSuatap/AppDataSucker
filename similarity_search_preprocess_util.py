89b1236c6148639f1ce426d3769abde41fb55e64
Arm <chayanin.suatap@gmail.com>
2020-02-16 21:20:47 +0700

    fix : sim search can't read transparent img

--- similarity_search_preprocess_util.py
+++ similarity_search_preprocess_util.py
@@ -4,16 +4,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 import shutil
 import random
+from PIL import Image
 
-MAX_CACHE = 100
-THRESHOLD = 5000
+
+MAX_CACHE = 20
+THRESHOLD = 7500
 COMPARE_NEXT_N = 20
+
+__background180 = Image.new('RGBA', (180, 180), (255,255,255))
+__background512 = Image.new('RGBA', (512, 512), (255,255,255))
 
 def loadimg(path, cache, img_fn):
     if img_fn in cache:
         return cache[img_fn]
     else:
-        img = plt.imread(path)
+        png = Image.open(path).convert('RGBA')
+        png = png.resize( (180, 180))
+        if png.size == (180, 180):
+            img = Image.alpha_composite(__background180, png)
+        elif png.size == (512, 512):
+            img = Image.alpha_composite(__background512, png)
+        img = np.array(img)[:,:,:3]
 
         #remove an image if exceed
         if len(cache.keys()) > MAX_CACHE:
@@ -30,12 +41,10 @@ def fn(path='icons.backup/'):
     cache = {}
     icon_fns = list(os.listdir(path))
     icon_fns_size = len(icon_fns)
+    error_image_files_n = 0
 
     #load saved progress
     # icon_fns = load_pickle('similarity_search/icon_fns.obj')
-
-    #new
-
 
     #old
     while len(icon_fns) > 0:
@@ -46,9 +55,11 @@ def fn(path='icons.backup/'):
         comparing_icon_fn = icon_fns[0]
         icon_fns.remove(comparing_icon_fn)
         current_icon_fns = [comparing_icon_fn]
+
         try:
             comparing_icon = loadimg(path + comparing_icon_fn, cache, comparing_icon_fn)
         except:
+            error_image_files_n += 1
             continue
     
         #compare to each other images
@@ -66,20 +77,23 @@ def fn(path='icons.backup/'):
                     break
 
             #simply skip if cant read img
-            except :
+            except Exception as e:
+                error_image_files_n += 1
                 remove_list.append(icon_fn)
+                print('this one is error', icon_fn)
+                print(repr(e))
 
         #copy file for test threshold
         print('copying')
+
         #copy for duplicate set
         set_dir = 'similarity_search/duplicate_set/' + str(len(current_icon_fns)) + comparing_icon_fn + '/'
         os.mkdir(set_dir)
-
         for x in current_icon_fns:
             try:
                 shutil.copyfile(path + x, set_dir + x)
             except Exception as e:
-                input(repr(e))
+                pass
 
         #copy for a unique from set
         shutil.copyfile(path + x, 'similarity_search/icons_remove_duplicate/' + random.choice(current_icon_fns))
@@ -91,10 +105,10 @@ def fn(path='icons.backup/'):
         #save icon_fns for run at home ..
         save_pickle(icon_fns, 'similarity_search/icon_fns.obj')
 
-        print('done copying')
+        print('done copying. error images n', error_image_files_n)
         print('name of comparer', comparing_icon_fn)
 
 
 
 if __name__ == '__main__':
-    fn()
+    fn('icons.combine.recrawled/')
