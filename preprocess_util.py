501c57e5e65b8384af2e487e83dcbec1cb3c755b
Arm <chayanin.suatap@gmail.com>
2020-02-17 14:35:40 +0700

    add : preprocess util : fn remove human icon and sc from dataset

--- preprocess_util.py
+++ preprocess_util.py
@@ -1,9 +1,12 @@
 import db_util
+import numpy as np
 import mypath
 from overall_feature_util import _extract_category
 import icon_util
 from global_util import save_pickle, load_pickle
 import os.path
+import os
+
 def prep_rating_category():
     conn = db_util.connect_db()
     app_ids_and_labels = []
@@ -79,18 +82,106 @@ def get_app_ids_without_icon(save_obj=False):
     if save_obj: save_pickle(app_ids_without_icon, 'app_ids_without_icon.recrawled.obj')
     return app_ids_without_icon
 
+def check_app_ids_for_human_test_obj_is_latest():
+    human_icons_app_ids = load_pickle('app_ids_for_human_test.obj')
+    human_icons_app_ids = [x[0] for x in human_icons_app_ids]
+    print(human_icons_app_ids)
+    from PIL import Image
+    import os
+    import numpy as np
+
+    pickle_images = []
+    human_test_images = []
+
+    __background512 = Image.new('RGBA', (512, 512), (255,255,255))
+
+    for app_id in human_icons_app_ids:
+        img_fn = app_id + '.png'
+        png = Image.open('icons.512/'+img_fn).convert('RGBA')
+        img = Image.alpha_composite(__background512, png)
+        img = np.array(img)[:,:,:3]
+        pickle_images.append(img)
+    
+    for img_fn in os.listdir('icons_human_test'):
+        png = Image.open('icons_human_test/'+img_fn).convert('RGBA')
+        img = Image.alpha_composite(__background512, png)
+        img = np.array(img)[:,:,:3]
+        human_test_images.append(img)
+
+    not_found_n = 0
+    
+    for pickle_img in pickle_images:
+        for human_img in human_test_images:
+            if np.sum(pickle_img - human_img) == 0:
+                break
+        else:
+            not_found_n += 1
+
+
+    print('ok', not_found_n)
+
+def remove_human_icons_from_dir(fd_path):
+    human_icons_app_ids = load_pickle('app_ids_for_human_test.obj')
+    human_icons_app_ids = [x[0] for x in human_icons_app_ids]
+
+    not_removed_n = 0
+
+    for app_id in human_icons_app_ids:
+        img_fn = app_id + '.png'
+        try:
+            os.remove(fd_path + '/' + img_fn)
+        except:
+            not_removed_n += 1
+    
+    print('not removed ', not_removed_n)
+
+def remove_human_scs_from_dir(fd_path):
+
+    human_icons_app_ids = load_pickle('app_ids_for_human_test.obj')
+    human_icons_app_ids = [x[0] for x in human_icons_app_ids]
+
+    del_app_n = 0
+
+    for human_sc_i, app_id in enumerate(human_icons_app_ids):
+
+        # human_sc = icon_util.load_icon_by_fn('screenshots_human_test/%d.png' % (human_sc_i,) , 256, 160, rotate_for_sc=True)
+
+        #search for exist file
+        exist_files = []
+        for i in range(21):
+            sc_fn = '%s%2d.png' % (app_id,i)
+            if os.path.exists(fd_path + sc_fn):
+                exist_files.append(fd_path + sc_fn)
+            
+        found = False
+        
+        for path in exist_files:
+            try:
+                os.remove(path)
+                found = True
+                print('remove', path)
+            except:
+                pass
+        
+        if found : del_app_n += 1
+    
+    print('del app n', del_app_n)
 
 if __name__ == '__main__':
+    
+    # remove_human_icons_from_dir('similarity_search/icons_rem_dup_recrawl/')
+    remove_human_scs_from_dir('screenshots.256.distincted/')
+
     # o = get_app_ids_without_icon(save_obj=True)
     # print(o, len(o))
-    app_ids_without_icon = load_pickle('app_ids_without_icon.recrawled.obj')
-    import sc_util
-    sc_dict = sc_util.make_sc_dict()
-    has_sc = []
-    for x in app_ids_without_icon:
-        if x in sc_dict and len(sc_dict[x]) > 0:
-            print(x)
-            has_sc.append(x)
-    print(has_sc, len(has_sc))
+    # app_ids_without_icon = load_pickle('app_ids_without_icon.recrawled.obj')
+    # import sc_util
+    # sc_dict = sc_util.make_sc_dict()
+    # has_sc = []
+    # for x in app_ids_without_icon:
+    #     if x in sc_dict and len(sc_dict[x]) > 0:
+    #         print(x)
+    #         has_sc.append(x)
+    # print(has_sc, len(has_sc))
     
 
