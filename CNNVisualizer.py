587f449335c794f965cdeff99a38502c30e3588a
Chayanin <arm.crazyplay@gmail.com>
2020-06-25 16:10:22 +0700

    change : plot feature 6 x 3 (same as filters)

--- CNNVisualizer.py
+++ CNNVisualizer.py
@@ -15,7 +15,7 @@ class CNNVisualizer:
             if 'conv' not in layer.name:
                 continue
             filters, biases = layer.get_weights()
-            print(layer.name, filters.shape)
+            print(layer.name, filters.shape, biases.shape)
             f_min, f_max = filters.min(), filters.max()
             filters = (filters - f_min) / (f_max - f_min)
 
@@ -43,43 +43,55 @@ class CNNVisualizer:
             img = load_icon_by_fn(img_path, 128, 128)
             img = img/255
             feature_maps = m.predict(np.array([img]))
+
+            print('layer.name', layer.name)
             print('output shape', feature_maps.shape)
 
             # plot all 64 maps in an 8x8 squares
-            square = 8
+            square = (6, 3)
             ix = 1
-            for _ in range(square):
-                for _ in range(square):
+            for _ in range(square[0]):
+                for _ in range(square[1]):
                     # specify subplot and turn of axis
-                    ax = plt.subplot(square, square, ix)
+                    ax = plt.subplot(square[0], square[1], ix)
                     ax.set_xticks([])
                     ax.set_yticks([])
                     # plot filter channel in grayscale
-                    plt.imshow(feature_maps[0, :, :, ix-1], cmap='gray')
+                    plt.imshow(feature_maps[0, :, :, ix-1], cmap = 'gray')
                     ix += 1
             # show the figure
             plt.show()
+        
 
 
 if __name__ == '__main__':
     model_path = 'sim_search_t/models/icon_model2.4_k3_t-ep-433-loss-0.319-acc-0.898-vloss-3.493-vacc-0.380.hdf5'
-    # img_path = ''
-    # v = CNNVisualizer(model_path)
-    # v.visualize_feature(layer_i = 1, img_path = 'icons.combine.recrawled/aceviral.dragoncraft.png')
+    v = CNNVisualizer(model_path)
+    # v.visualize_filter()
+    v.visualize_feature(layer_i = 1, img_path = 'icons.combine.recrawled/com.bigdaddyapp.android.reverse.png')
 
-    aial = load_pickle('aial_seed_327.obj')
-    m = load_model(model_path)
-    aial_train, aial_test = keras_util.gen_k_fold_pass(aial, kf_pass=3, n_splits=4)
-    app_id_pred_d = []
-    for x in aial_test:
-        app_id = x[0]
-        img_path = 'icons.combine.recrawled/' + app_id + '.png'
-        normed_img = load_icon_by_fn(img_path, 128, 128)/255
-        pred = m.predict(np.array([normed_img]))
-        conf = max(pred[0])
-        app_id_pred_d.append( (app_id,conf))
-    sorted_preds = sorted(app_id_pred_d, key = lambda x : x, reverse=True)
-    print(sorted_preds[:10])
+    # aial = load_pickle('aial_seed_327.obj')
+    # m = load_model(model_path)
+    # aial_train, aial_test = keras_util.gen_k_fold_pass(aial, kf_pass=3, n_splits=4)
+    # app_id_pred_d = []
+    # for x in aial_test:
+    #     app_id = x[0]
+    #     print(app_id)
+    #     img_path = 'icons.combine.recrawled/' + app_id + '.png'
+    #     normed_img = load_icon_by_fn(img_path, 128, 128)/255
+    #     pred = m.predict(np.array([normed_img]))
+    #     conf = max(pred[0])
+    #     app_id_pred_d.append( (app_id,conf))
+    # sorted_preds = sorted(app_id_pred_d, key = lambda x : x[1], reverse=True)
+    # print(sorted_preds[:10])
+    # save_pickle(sorted_preds, 'sorted_preds_icon_model2.4_k3.obj')
+
+    obj = load_pickle('sorted_preds_icon_model2.4_k3.obj')
+    count = 0
+    for app_id, conf in obj:
+        if conf <= 0.75 and conf >= 0.7:
+            print(app_id)
+
 
     
 
