ae227fcf647dff580d9638ff3478126cd3d9c9b9
Chayanin <arm.crazyplay@gmail.com>
2020-11-22 11:56:13 +0700

    add : icon overall generator

--- icon_overall_di.py
+++ icon_overall_di.py
@@ -4,15 +4,19 @@ from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.layers import Dense, concatenate, Dropout, LeakyReLU, BatchNormalization
 from tensorflow.keras.activations import linear
 from keras.utils.vis_utils import plot_model
+import global_util
+import random
+import keras_util
+from image_batch_generator import image_batch_sequence
 
-def make_icon_overall_model(icon_model_path, dropout_rate=0.5, overall_make_model_param={}):
+
+def make_icon_overall_model(icon_model, dropout_rate=0.5, overall_make_model_param={}):
     overall_model = overall.make_model(**overall_make_model_param)
-    #rename overall layers
+    # rename overall layers
     for layer in overall_model.layers:
         layer._name = 'overall_'+layer.name
     overall_model.compile(loss='categorical_crossentropy', metrics=['acc'])
 
-    icon_model = load_model(icon_model_path)
     icon_model = di.extend_cate_model(icon_model)
 
     # drop last layer of overall and icon
@@ -25,7 +29,8 @@ def make_icon_overall_model(icon_model_path, dropout_rate=0.5, overall_make_mode
     x = Dropout(dropout_rate,  name='icon_overall_dropout')(concat)
     x = LeakyReLU(name='icon_overall_leakyrelu')(x)
     x = BatchNormalization(name='icon_overall_batchnormalization')(x)
-    output_layer = Dense(2, activation='softmax', name='icon_overall_output')(x)
+    output_layer = Dense(2, activation='softmax',
+                         name='icon_overall_output')(x)
 
     new_model = Model(inputs=[
         icon_model.layers[0].input,
@@ -33,7 +38,7 @@ def make_icon_overall_model(icon_model_path, dropout_rate=0.5, overall_make_mode
         overall_model.layers[1].input,
         overall_model.layers[2].input,
         overall_model.layers[3].input,
-        ],
+    ],
         outputs=output_layer
     )
     new_model.compile(optimizer='adam',
@@ -43,8 +48,32 @@ def make_icon_overall_model(icon_model_path, dropout_rate=0.5, overall_make_mode
 
 if __name__ == '__main__':
     overall_make_model_param = {
-        'cate_nodes_size': 17, 'sdk_version_nodes_size': 34, 'content_rating_nodes_size': 34}
+        'cate_nodes_size': 17, 'sdk_version_nodes_size': 38, 'content_rating_nodes_size': 38, 'other_input_nodes_size' : 9}
     model = make_icon_overall_model(
         icon_model_path='sim_search_t/models/icon_model2.4_k0_t-ep-404-loss-0.318-acc-0.896-vloss-3.674-vacc-0.357.hdf5',
         overall_make_model_param=overall_make_model_param
     )
+
+    aial = global_util.load_pickle('aial_seed_327.obj')
+    app_ids_d = {x[0]: True for x in aial}
+    data = di._prepare_dataset(
+        app_ids_d,
+        'crawl_data/first_version/data.db',
+        'crawl_data/update_first_version_2020_09_12/data.db')
+    random.seed(5)
+    random.shuffle(data)
+    train_data, test_data = keras_util.gen_k_fold_pass(
+        data, kf_pass=0, n_splits=4)
+    app_id_overall_feature_d = overall.make_app_id_overall_feature_d(
+        data,
+        'crawl_data/first_version/data.db')
+    # class weight
+    cw = di.compute_class_weight([x[1] for x in train_data])
+    print('class weight', cw)
+    # sequences
+    train_seq = image_batch_sequence(
+        train_data, batch_size=8, app_id_overall_feature_d=app_id_overall_feature_d)
+    test_seq = image_batch_sequence(
+        test_data, batch_size=8, shuffle=False, app_id_overall_feature_d=app_id_overall_feature_d)
+
+    model.fit(train_seq, epochs=1, batch_size=8)
