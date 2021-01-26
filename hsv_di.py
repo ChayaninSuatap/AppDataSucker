f0f22b155c4f6937e80ab879507b8c1460791e96
Chayanin <arm.crazyplay@gmail.com>
2021-01-26 22:43:19 +0700

    wip make model

--- hsv_di.py
+++ hsv_di.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import rgb_to_hsv
 from icon_util import open_and_resize
 
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.layers import Dense, Conv2D, Input, BatchNormalization, Dropout, LeakyReLU, concatenate
+
 def make_feature(fn, bin=10):
     img = Image.open(fn)
     width ,height = img.size
@@ -28,6 +31,23 @@ def make_feature(fn, bin=10):
     feature = np.concatenate((hue_norm,sat_norm,val_norm))
     return feature
 
+def make_model(input_size, denses=[8,4]):
+    input_layer = Input(shape=(input_size,))
+    x = Dense(10)(input_layer)
+    x = LeakyReLU()(x)
+    x = Dense(5)(x)
+    x = LeakyReLU()(x)
+    x = Dense(2, activation='softmax')(x)
+
+    def add_dense():
+    
+    model = Model(inputs=input_layer, outputs=x)
+    model.compile(loss='categorical_crossentropy', metrics=['acc'])
+    return model
+    
+
 if __name__ == '__main__':
-    feature = make_feature('allcolor.jpg', 5)
-    print(feature)
+    # feature = make_feature('allcolor.jpg', 5)
+    # print(feature)
+
+    m = make_model(10)
