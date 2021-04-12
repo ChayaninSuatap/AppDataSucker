549b54b646d8df6761885115fc836a93f0e7e7eb
Chayanin <arm.crazyplay@gmail.com>
2021-04-12 22:20:33 +0700

    change output to be 2 nodes

--- predict_human_acc_binary.py
+++ predict_human_acc_binary.py
@@ -26,7 +26,7 @@ def mod_model(model, freeze, denses=[16]):
     for level, dense in enumerate(denses):
         x = add_dense(dense, x, level=level)
 
-    output_layer = Dense(1, activation='softmax')(x)
+    output_layer = Dense(2, activation='softmax')(x)
 
     model = Model(input_layer.input, output_layer)
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
@@ -53,14 +53,16 @@ def make_icon_dataset(icon_human_acc_obj, icon_dir = 'icons_human_test/'):
     output = []
     for icon_fn, avg_human_acc in icon_human_acc_obj.items():
         icon = icon_util.load_icon_by_fn(icon_dir + icon_fn, 128, 128) / 256
-        output.append( (icon, avg_human_acc))
+        label = np.array([0, 1]) if avg_human_acc == 0 else np.array([1, 0])
+        output.append( (icon, label))
     return output
 
 def make_sc_dataset(sc_human_acc_obj, sc_dir = 'screenshots_human_test/'):
     output = []
     for sc_fn, avg_human_acc in sc_human_acc_obj.items():
         sc = icon_util.load_icon_by_fn(sc_dir + sc_fn, 256, 160, rotate_for_sc=True) / 256
-        output.append( (sc, avg_human_acc))
+        label = np.array([0, 1]) if avg_human_acc == 0 else np.array([1, 0])
+        output.append( (sc, label))
     return output
 
 def best_epoch_by_val(history, val, metrics):
@@ -78,22 +80,29 @@ def best_epoch_by_val(history, val, metrics):
     return output
 
 if __name__ == '__main__':
-    # freeze = True
-    # denses = [8]
-    # model = load_model('sim_search_t/models/icon_model2.4_k3_t-ep-433-loss-0.319-acc-0.898-vloss-3.493-vacc-0.380.hdf5')
-    # model = mod_model(model, freeze, denses)
+    epochs = 50
+    batch_size = 16
+    freeze = True
+    denses = [8]
+    model = load_model('sim_search_t/models/icon_model2.4_k3_t-ep-433-loss-0.319-acc-0.898-vloss-3.493-vacc-0.380.hdf5')
+    model = mod_model(model, freeze, denses)
     # model.summary()
 
     # make_sc_human_acc_obj_from_lines()
-    # app_id_bin_acc_d = global_util.load_pickle('journal/pred_human_acc/icon_human_acc_bin.obj')
-    # dat = make_icon_dataset(app_id_bin_acc_d)
-    # global_util.save_pickle(dat, 'journal/pred_human_acc/icon_feature_human_acc_bin.obj')
+    app_id_bin_acc_d = global_util.load_pickle('journal/pred_human_acc/icon_human_acc_bin.obj')
+    dat = make_icon_dataset(app_id_bin_acc_d)
+    global_util.save_pickle(dat, 'journal/pred_human_acc/icon_feature_human_acc_bin.obj')
 
     app_id_bin_acc_d = global_util.load_pickle('journal/pred_human_acc/sc_human_acc_bin.obj')
     dat = make_sc_dataset(app_id_bin_acc_d)
-    # global_util.save_pickle(dat, 'journal/pred_human_acc/sc_feature_human_acc_bin.obj')
+    global_util.save_pickle(dat, 'journal/pred_human_acc/sc_feature_human_acc_bin.obj')
+
+    input('done')
 
     for k_iter in range(4):
         x_train, y_train, x_test, y_test = keras_util.gen_k_fold_pass_as_np(aial=dat, kf_pass=k_iter, n_splits=4)
-        print(np.unique(y_test, return_counts=True))
+        # print(np.unique(y_test, return_counts=True))
+        history = model.fit(x=x_train, y=y_train, validation_data=(x_test, y_test),
+        epochs=epochs, batch_size=batch_size, verbose=1)
+        input(best_epoch_by_val(history, 'val_acc', ['val_acc']))
 
